b481f9b15987e6f0d78d1d05653200b1ea5f6d39
Tiago Lima <tiago.lima@osf.digital>
2020-04-29 19:43:00 -0300

    Added Styles.

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,46 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import * as TodoActions from "../store/actions/todos";
+import { Creators as TodoActions } from "../../store/ducks/todos";
 
-import './styles.css';
+import "./styles.css";
 
 function TodoList(props) {
-    const { todos, toggleTodo, removeTodo } = props;
+  const [name, setName] = useState("");
+  const { todos, toggleTodo, removeTodo } = props;
 
-    function handleSubmit = e => {
-        e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.addTodo(name);
+    setName("");
+  };
 
-        props.addTodo(input.value);
-
-        input.value = '';
-    }
-
-    return (
-        <section>
-            <form onSubmit={handleSubmit}>
-                <input ref={el => input = e} />
-                <button type="submit">Novo</button>
-            </form>
-            <ul>
-                {todos.map(todo => (
-                    <li key={todo.id}>
-                        {todo.complete ? <s>{todo.text}</s> : todo.text}
-                        <div>
-                            <button onClick={() => toggleTodo(todo.id)}>Toggle</button>
-                            <button onClick={() => removeTodo(todo.id)}>Remove</button>
-                        </div>
-                    </li>
-                ))}
-            </ul>
-        </section>
-    )
+  return (
+    <section>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button type="submit">Novo</button>
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.complete ? <s>{todo.text}</s> : todo.text}
+            <div>
+              <button onClick={() => toggleTodo(todo.id)}>Toggle</button>
+              <button onClick={() => removeTodo(todo.id)}>Remove</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
 
-const mapStateToProps = state => ({
-    todos: state.todos
+const mapStateToProps = (state) => ({
+  todos: state.todos,
 });
 
-const mapDispatchToProps = dispatch => bindActionCreators(TodoActions, dispatch);
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators(TodoActions, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
